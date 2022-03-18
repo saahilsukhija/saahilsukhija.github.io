e2c0f750583bea3cd83c505f9f1370d55a805cf0
Saahil Sukhija <saahilsukhija@gmail.com>
2022-03-17 23:20:36 -0700

    Beta

---
+++ navbar.js
@@ -0,0 +1,8 @@
+// function hamburgerClicked() {
+// 	var x = document.getElementById("mobile-links");
+// 	if (x.style.display === "block") {
+// 		x.style.display = "none";
+// 	} else {
+// 		x.style.display = "block";
+// 	}
+// }
